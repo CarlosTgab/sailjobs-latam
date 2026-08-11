a6274287a9ba2a55b072857f001714b6b9c0ab1c
Carlos Gabasio <gabasiocarli@gmail.com>
2026-08-11 18:48:46 -0300

    Corrige validacion de identificadores UUID

--- src/utils/jobsStorage.js
+++ src/utils/jobsStorage.js
@@ -31,7 +31,7 @@ function safeDispatchJobsChanged() {
 }
 
 function isUuid(value) {
-    return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{12}$/i.test(
+    return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
         String(value || "")
     );
 }
